a66554383fc726c4e6e48c7954cc9dc923f53305
FangYu199 <talentyupush@163.com>
2020-11-20 12:07:49 +0800

    update Open link method

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3ab11ebb3d053ab44c943bee20891ee9.js"
+  "./precache-manifest.43ac62849dda7626a0a900c58ecbb5f4.js"
 );
 
 self.addEventListener('message', (event) => {
